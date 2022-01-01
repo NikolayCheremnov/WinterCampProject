2051dc291a742ee25365ec659ea811729a9bdd1b
NikolayCheremnov <coolnick1999@gmail.com>
2022-01-01 23:06:58 +0700

    first window creating

---
+++ Sources/GameSettings.py
@@ -0,0 +1,4 @@
+# game constants
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+SCREEN_TITLE = 'Test'
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('Hello world')
+from Sources.GameSettings import SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE
+from Sources.MenuWindow import MenuWindow
+
+if __name__ == '__main__':
+    # test screen viewing
+    window = MenuWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window.setup()
+    window.run()
