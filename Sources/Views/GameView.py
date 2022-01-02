14fd4e09678d1e51290d169c1d5f2c0d83a93635
NikolayCheremnov <coolnick1999@gmail.com>
2022-01-02 16:44:50 +0700

    Scene added to GameView.

--- Sources/Views/GameView.py
+++ Sources/Views/GameView.py
@@ -13,9 +13,8 @@ class GameView(arcade.View):
         # call super method
         super().__init__()
 
-        # sprites lists
-        self.wall_list = None
-        self.player_list = None
+        # scene
+        self.scene = None
 
         # special variable for player sprite
         self.player_sprite = None
@@ -24,16 +23,19 @@ class GameView(arcade.View):
         """
             Setup game here: you need call this method before game has been run
         """
-        # sprite lists creating
-        self.player_list = arcade.SpriteList()
-        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+        # Initialize Scene
+        self.scene = arcade.Scene()
+
+        # Create the Sprite lists
+        self.scene.add_sprite_list("Player")
+        self.scene.add_sprite_list("Walls", use_spatial_hash=True)
 
         # player setting up
         image_source = ':resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png'
         self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
         self.player_sprite.center_x = 64
         self.player_sprite.center_y = 128
-        self.player_list.append(self.player_sprite)
+        self.scene.add_sprite("Player", self.player_sprite)
 
         # Create the ground
         # This shows using a loop to place multiple sprites horizontally
@@ -41,7 +43,7 @@ class GameView(arcade.View):
             wall = arcade.Sprite(":resources:images/tiles/grassMid.png", TILE_SCALING)
             wall.center_x = x
             wall.center_y = 32
-            self.wall_list.append(wall)
+            self.scene.add_sprite("Walls", wall)
 
         # Put some crates on the ground
         # This shows using a coordinate list to place sprites
@@ -49,11 +51,9 @@ class GameView(arcade.View):
 
         for coordinate in coordinate_list:
             # Add a crate on the ground
-            wall = arcade.Sprite(
-                ":resources:images/tiles/boxCrate_double.png", TILE_SCALING
-            )
+            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", TILE_SCALING)
             wall.position = coordinate
-            self.wall_list.append(wall)
+            self.scene.add_sprite("Walls", wall)
 
     def on_draw(self):
         """
@@ -63,6 +63,5 @@ class GameView(arcade.View):
         # clear screen
         arcade.start_render()
 
-        # draw all sprites
-        self.wall_list.draw()
-        self.player_list.draw()
+        # Draw our Scene
+        self.scene.draw()
